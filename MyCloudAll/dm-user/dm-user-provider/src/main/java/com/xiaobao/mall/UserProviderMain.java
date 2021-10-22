616e278177b717eb4b64ef51ad5d72bcc962a3b0
Diana <Jxiuye@163.com>
2021-10-22 11:30:03 +0800

    # 上传一下

---
+++ MyCloudAll/dm-user/dm-user-provider/src/main/java/com/xiaobao/mall/UserProviderMain.java
@@ -0,0 +1,11 @@
+package com.xiaobao.mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserProviderMain {
+    public static void main(String[] args) {
+        SpringApplication.run(UserProviderMain.class, args);
+    }
+}
